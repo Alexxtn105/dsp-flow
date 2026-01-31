d4ad134195fb0bb173f49232962ae9085f905e7c
AlexxTN <alexxtn@mail.com>
2026-01-31 13:07:39 +0300

    background

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { ReactFlow, Controls } from '@xyflow/react';
+import { useState } from 'react';
+import { ReactFlow, Background, Panel } from '@xyflow/react';
+
 import '@xyflow/react/dist/style.css';
 
-import { defaultNodes } from './nodes.jsx';
+import { defaultNodes } from './nodes';
 import { defaultEdges } from './edges';
 
 function Flow() {
+    const [variant, setVariant] = useState('cross');
+
     return (
         <ReactFlow defaultNodes={defaultNodes} defaultEdges={defaultEdges} fitView>
-            <Controls />
+            <Background color="skyblue" variant={variant} />
+            <Panel>
+                <div>variant:</div>
+                <button onClick={() => setVariant('dots')}>dots</button>
+                <button onClick={() => setVariant('lines')}>lines</button>
+                <button onClick={() => setVariant('cross')}>cross</button>
+            </Panel>
         </ReactFlow>
     );
 }
